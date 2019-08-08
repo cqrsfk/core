a5f91746b38b741eb3eebdaafbab9daadb1ac7ee
liangzeng <1405491181@qq.com>
2019-08-08 11:01:54 +0800

    add license

--- src/Saga.ts
+++ src/Saga.ts
@@ -11,15 +11,12 @@ export class Saga extends Actor {
     const actor = await this.$cxt.get<T>(type, id);
     if (!actor) return null;
     await actor.$lock(this._id);
-    this.$cxt.apply("add", {
+    this.actorInfo = Object.assign({}, this.actorInfo, {
       [actor._id]: { type: actor.$type, rev: actor._rev }
     });
+
     await this.save();
     return actor as T;
-  }
-
-  addHandle({ data }) {
-    this.actorInfo = Object.assign({}, this.actorInfo, data);
   }
 
   async end() {
@@ -28,8 +25,12 @@ export class Saga extends Actor {
       const actor = await this.$cxt.get(type, aid);
       await actor.$unlock(this._id);
     }
-    this.finished = true;
     await this.save();
+    this.$cxt.apply("finish", []);
+  }
+
+  finish() {
+    this.finished = true;
   }
 
   async recover() {
